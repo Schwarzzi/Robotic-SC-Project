d51c557cbdf6d51312b0c361737a7f114a566e44
Gad Marconi <gadmarconi4@outlook.com>
2024-01-10 10:42:37 +0100

    Refactor stress_analysis function to accept forces as a dictionary

--- Structures/shear.py
+++ Structures/shear.py
@@ -2,22 +2,30 @@ from geometry import make_geometry
 from sectionproperties.analysis import Section
 
 
-def stress_analysis(poly: Section, mode: str="shear", *args):
+def stress_analysis(poly: Section, mode: str="zxy", forces: dict={'n': 100e3}):
     """
     Perform stress analysis on the section.
 
     Parameters:
         poly (Section): The section to perform the stress analysis on.
         mode (str): The type of stress analysis to perform.
-        *args: Additional arguments to pass to the stress analysis function.
+        Forces: Additional arguments to pass to the stress analysis function.
+            n (float)  Axial force
+            vx (float)  Shear force acting in the x-direction
+            vy (float) Shear force acting in the y-direction
+            mxx (float) Bending moment about the centroidal xx-axis
+            myy (float) Bending moment about the centroidal yy-axis
+            m11 (float) Bending moment about the centroidal 11-axis
+            m22 (float) Bending moment about the centroidal 22-axis
+            mzz (float) Torsion moment about the centroidal zz-axis
 
     Returns:
         None
     
     """
     section = poly
-    s = section.calculate_stress(vx=10e3, vy=10e3)
+    s = section.calculate_stress(**forces)
     s.plot_stress(stress=mode, cmap='viridis', normalize=True)
 
-poly = make_geometry(display=True)
-stress_analysis(poly, mode="zxy")
+poly = make_geometry(display=False)
+stress_analysis(poly)
