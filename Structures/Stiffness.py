cffa226dc25abe1936f235648e65f9b31612820d
SiNicolas <n.situm@student.tudelft.nl>
2024-01-12 14:41:10 +0100

    stiffness

--- Structures/Stiffness.py
+++ Structures/Stiffness.py
@@ -7,9 +7,9 @@ L = 5
 Imm = (75**4/12 - 71**4/12) + 584*805.4**2 + (465*2**3)/12 + 465*2*806.4**2 + (2*930**3*np.sin(60)**2)/12 + 930*2*(0.5*np.sin(60)*930)**2
 I = Imm*10**(-12)
 
-f_nat_lat = 0.25*np.sqrt(A*E/(m*L))
+f_nat_ax = 0.25*np.sqrt(A*E/(m*L))
 
-f_nat_ax = 0.56*np.sqrt(E*I/(m*L**3))
+f_nat_lat = 0.56*np.sqrt(E*I/(m*L**3))
 
-print(f_nat_lat)
-print(f_nat_ax)
+print(f"lateral = {f_nat_lat}")
+print(f"axial = {f_nat_ax}")
