cb8ade76aea9433441c2df7af7d421ccb943b359
SiNicolas <n.situm@student.tudelft.nl>
2024-01-13 14:45:03 +0100

    stiffness sdof

--- Structures/Stiffness.py
+++ Structures/Stiffness.py
@@ -1,20 +1,24 @@
 import numpy as np
 
-#Bus structure frequency
+
+#Bus structure frequency mdof
 
 A = (584 + 465*2 + 930*2)*4/(1000**2)
 E = 210*10**9
 m = 3000
 L = 5
 Imm = (75**4/12 - 71**4/12) + 584*805.4**2 + (465*2**3)/12 + 465*2*806.4**2 + (2*930**3*np.sin(60)**2)/12 + 930*2*(0.5*np.sin(60)*930)**2
-I = Imm*10**(-12)
+I = 4*Imm*10**(-12)
 
 f_nat_ax_b = 0.25*np.sqrt(A*E/(m*L))
 
 f_nat_lat_b = 0.56*np.sqrt(E*I/(m*L**3))
 
+f_nat_sdof = 1/(2*np.pi) * np.sqrt(3*E*I/ (m*L**3))
+
 print(f"lateral = {f_nat_lat_b}")
 print(f"axial = {f_nat_ax_b}")
+print(f"sdof = {f_nat_sdof}")
 
 ###########
 #solar array frequency
