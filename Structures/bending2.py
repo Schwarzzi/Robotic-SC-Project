327717a8a4d5a2d9dcc28fab7060106fe7b923fd
SiNicolas <n.situm@student.tudelft.nl>
2024-01-18 11:58:01 +0100

    iteration

--- Structures/bending2.py
+++ Structures/bending2.py
@@ -7,9 +7,11 @@ DL_sf = DL*sf
 
 w =  DL_sf / 5
 E = 210*10**9
-I = 0.0010930373*4
-
+#I = 0.0010930373*4
+I = 0.008678310387
 
 d = w*L**4 / (8*E*I)
 
 print(d*1000)
+
+#thickness needed to prevent bending problems
